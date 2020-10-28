fbe987e0525932058d04400fc44efb5b5270ca57
Johannes Hofmeister <github@account.cessor.de>
2020-10-28 12:32:09 +0100

    Stylnechnagnes

---
+++ src/blog/tests/test_view_article_detail.py
@@ -0,0 +1,119 @@
+import re
+from django.test import TestCase
+from blog.models import Author, Article
+from bs4 import BeautifulSoup
+
+
+def collapse_whitespace(text):
+    pattern = re.compile(r'\s+')
+    return pattern.sub(' ', text).strip()
+
+
+class ArticleTests(TestCase):
+    def test_one_author(self):
+        # Arrange: Authors
+        lars = Author.objects.create(
+            username='larscov2',
+            first_name='Lars'
+        )
+
+        # Arrange: Article
+        article = Article.objects.create(
+            title='Creating a Binary Search in Python',
+            content='Lorem Ipsum'
+        )
+
+        # Arrange: Add Authorship
+        article.authors.add(lars)
+        article.save()
+
+        # Act, System under Test
+        response = self.client.get(article.get_absolute_url())
+
+        soup = BeautifulSoup(response.content, 'lxml')
+        authors = soup.find('span', attrs={'class': 'authors'})
+        authors = collapse_whitespace(authors.text)
+
+        # Assert: Authors are listed correctly
+        self.assertEqual(
+            authors,
+            'Geschreiben von Lars.'
+        )
+
+    def test_two_authors(self):
+        # Arrange: Authors
+        lars = Author.objects.create(
+            username='larscov2',
+            first_name='Lars'
+        )
+
+        horst = Author.objects.create(
+            username='shorst',
+            first_name='Horst'
+        )
+
+        # Arrange: Article
+        article = Article.objects.create(
+            title='Creating a Binary Search in Python',
+            content='Lorem Ipsum'
+        )
+
+        # Arrange: Add Authorship
+        article.authors.add(lars)
+        article.authors.add(horst)
+        article.save()
+
+        # Act, System under Test
+        response = self.client.get(article.get_absolute_url())
+
+        soup = BeautifulSoup(response.content, 'lxml')
+        authors = soup.find('span', attrs={'class': 'authors'})
+        authors = collapse_whitespace(authors.text)
+
+        # Assert: Authors are listed correctly
+        self.assertEqual(
+            authors,
+            'Geschreiben von Lars und Horst.'
+        )
+
+    def test_more_than_two_authors(self):
+        # Arrange: Authors
+        lars = Author.objects.create(
+            username='larscov2',
+            first_name='Lars'
+        )
+
+        horst = Author.objects.create(
+            username='shorst',
+            first_name='Horst'
+        )
+
+        johannes = Author.objects.create(
+            username='hjohannes',
+            first_name='Johannes'
+        )
+
+        # Arrange: Article
+        article = Article.objects.create(
+            title='Creating a Binary Search in Python',
+            content='Lorem Ipsum'
+        )
+
+        # Arrange: Add Authorship
+        article.authors.add(lars)
+        article.authors.add(horst)
+        article.authors.add(johannes)
+        article.save()
+
+        # Act, System under Test
+        response = self.client.get(article.get_absolute_url())
+
+        soup = BeautifulSoup(response.content, 'lxml')
+        authors = soup.find('span', attrs={'class': 'authors'})
+        authors = collapse_whitespace(authors.text)
+
+        # Assert: Authors are listed correctly
+        self.assertEqual(
+            authors,
+            'Geschreiben von Lars, Horst und Johannes.'
+        )
