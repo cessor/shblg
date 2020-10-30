cf2216362112de99fa24d1cf724286cac257844b
Johannes Hofmeister <github@account.cessor.de>
2020-10-30 14:29:00 +0100

    Shnomain chnage dasasdf

--- src/shit/settings.py
+++ src/shit/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 ALLOWED_HOSTS = [
     'localhost',
-    'shit.kumbier.it'
+    'random.kumbier.it'
 ]
 
 
