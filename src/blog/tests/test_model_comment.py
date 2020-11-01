e4d486d28ba34740128a1e7e6be9716c962d96bf
Johannes Hofmeister <github@account.cessor.de>
2020-11-02 00:03:20 +0100

    Added test stub

--- src/blog/tests/test_model_comment.py
+++ src/blog/tests/test_model_comment.py
@@ -1,1 +1,17 @@
-raise NotImplementedError()
+from django.test import TestCase
+from blog.models import Article, Author, Comment
+
+class CommentTests(TestCase):
+    def test_comment(self):
+        writer = Author.objects.create(
+            username='Horst',
+        )
+
+        commenter = Author.objects.create(
+            username='Johannes'
+        )
+
+        Article.objects.create(
+
+        )
+
