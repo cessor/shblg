c1aa9b3426412d4b773b14615bc27a8f97c316ad
Johannes Hofmeister <github@account.cessor.de>
2020-11-02 19:17:32 +0100

    Extended test

--- src/blog/tests/test_view_authors.py
+++ src/blog/tests/test_view_authors.py
@@ -17,19 +17,26 @@ class AuthorTests(TestCase):
         # Assert
         self.assertEqual(url, '/author/johannes')
 
-    def test_author_page_shows_authors_name(self):
+    def test_author_page(self):
         # Arrange, System under Test
         author = Author.objects.create(
             username='johannes',
             first_name='Johannes',
-            last_name='Hofmeister'
+            last_name='Hofmeister',
+            biography='Hello, World!'
         )
 
         # Act
         response = self.client.get(author.get_absolute_url())
 
-        # Assert
+        # Assert: Name on page
         self.assertInHTML(
             '<h1>Johannes</h1>',
+            response.content.decode('utf-8')
+        )
+
+        # Assert: Bio on page
+        self.assertInHTML(
+            '<p>Hello, World!</p>',
             response.content.decode('utf-8')
         )
